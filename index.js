b44e3274f6d5fc93924c78e72fcd4c902b2fab4b
azad1036 <mohammadazadhossain2024@gmail.com>
2025-05-05 15:45:37 +0600

    RoktoNet Server Setup

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// MedleWare
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const uri = "mongodb://localhost:27017/";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function runDB() {
+  const roktoNetDB = client.db("roktoNetDB");
+  const usersDBCollection = roktoNetDB.collection("UsersDB");
+
+  try {
+    // user register API
+    app.post("/users", async (req, res) => {
+      const users = req.body;
+      console.log(users);
+      const storeUsers = await usersDBCollection.insertOne(users);
+      res.send(storeUsers);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+runDB();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
